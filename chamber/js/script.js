5572be314838afc364c3883ca2542fc99df63198
Use your GitHub username <Enter your email address>
2022-10-07 09:58:31 -0600

    Quick fixes

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -6,10 +6,11 @@ function toggleMenu() {
 const x = document.getElementById("hamburgerBtn")
 x.onclick = toggleMenu;
 
-const today = new Date();
-document.querySelector("header div span").textContent = today.getFullYear();
-document.getElementById("todaydate").textContent = document.lastModified;
+const date_area = document.querySelector('#todaydate')
+const date_now = new Date();
+const full_date = new Intl.DateTimeFormat("en-US", { dateStyle: "full"}).format(date_now);
+date_area.innerHTML = `<em>${full_date}</em>`
 
-const update = new Date();
+const today = new Date();
 document.querySelector("footer div span").textContent = today.getFullYear();
 document.getElementById("lastupdatedate").textContent = document.lastModified;
