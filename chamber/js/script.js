ed228205234281fe8751975582c48e4e4b797a3d
Use your GitHub username <Enter your email address>
2022-10-12 16:53:56 -0600

    Added banner and button

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -11,6 +11,16 @@ const date_now = new Date();
 const full_date = new Intl.DateTimeFormat("en-US", { dateStyle: "full"}).format(date_now);
 date_area.innerHTML = `<em>${full_date}</em>`
 
+msg = document.querySelector("#msg-box")
+date = new Date()
+day = date.getDay()
+
+if (day == 1 || day == 2) {
+
+msg.style.display = "block"
+
+}
+
 const today = new Date();
 document.querySelector("footer div span").textContent = today.getFullYear();
 document.getElementById("lastupdatedate").textContent = document.lastModified;
