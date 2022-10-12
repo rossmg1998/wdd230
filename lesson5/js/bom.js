f31f26add1aafa3d50061e4d9b78d1379a6ef51e
Use your GitHub username <Enter your email address>
2022-10-11 18:03:28 -0600

    Adjusted js

--- lesson5/js/bom.js
+++ lesson5/js/bom.js
@@ -2,23 +2,45 @@ const list = document.querySelector('ul');
 const input = document.querySelector('input');
 const button = document.querySelector('button');
 
+// function doMyButtonThing(){}
+
 button.addEventListener('click', () => {
-    const myItem = input.value;
+	const myItem = input.value;
     input.value = '';
 
-    const listItem = document.createElement('li');
+// Create an li (and span) element
+	const listItem = document.createElement('li');
     const listText = document.createElement('span');
-    const listBtn = document.createElement('button');
 
+// Create a delete button
+	const listBtn = document.createElement('button');
+
+// Populate the li elements textContent or innerHTML with the input
     listItem.appendChild(listText);
     listText.textContent = myItem;
+
+// Append the li element with the delete button
     listItem.appendChild(listBtn);
-    listBtn.textContent = 'Delete';
-    list.appendChild(listItem);
 
+// Populate the button textContent with an ❌
+    listBtn.textContent = "❌";
+
+// Append the list element with the li element just created
+// and appended with text and the delete button
+	list.appendChild(listItem);
+
+// Add an event listener to the delete button that removes 
+// the li element when clicked
     listBtn.addEventListener('click', () => {
-        list.removeChild(listItem);
-    });
+		list.removeChild(listItem);
+	});
 
-    input.focus();
+// Send the focus to the input element
+	input.focus();
+
+// Clean up the successful add of a chapter by changing the 
+// input to nothing or the empty string and setting the focus 
+// to the input
+// myInput.value="";
+
 });
