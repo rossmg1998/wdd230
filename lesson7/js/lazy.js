5efebb4a6e04e612d630edece1a629b7b5d7dcda
Use your GitHub username <Enter your email address>
2022-10-25 16:05:37 -0600

    W07 Activity

---
+++ lesson7/js/lazy.js
@@ -0,0 +1,38 @@
+// Gather the images to load
+let imagesToLoad = document.querySelectorAll("img[data-src]");
+
+// Set up the load images function which switches the src and the data-src attributes
+const loadImages = (image) => {
+image.setAttribute("src", image.getAttribute("data-src"));
+image.onload = () => {
+    image.removeAttribute("data-src");
+};
+};
+
+// Load all the images
+// imagesToLoad.forEach((img) => {
+// loadImages(img);
+// });
+
+// Add an intersection observer if supported by the browser
+if ("IntersectionObserver" in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+            if (item.isIntersecting) {
+                loadImages(item.target);
+                observer.unobserve(item.target);
+            }
+        });
+    });
+
+    imagesToLoad.forEach((img) => {
+        observer.observe(img);
+    });
+}
+
+// If the browser doesn't support intersection observers, then just load the images
+else {
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+});
+}
