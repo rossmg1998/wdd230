77b1ecd55f024c09ec994a03a3902789a8bc4564
Use your GitHub username <Enter your email address>
2022-10-25 20:39:34 -0600

    Number of visits to the page

--- chamber/js/discover.js
+++ chamber/js/discover.js
@@ -1,3 +1,4 @@
+/*
 // window.localStorage.setItem("last-visit", new Date("09/25/2022"));
 
 const visit = document.querySelector("#visit-message");
@@ -14,3 +15,25 @@ if (lastVisitString != null){
 
 visit.textContent = visitMessage;
 window.localStorage.setItem("last-visit", present.toString());
+*/
+
+// initialize display elements
+const todayDisplay = document.querySelector(".today");
+const visitsDisplay = document.querySelector(".visits");
+
+// get the stored value in localStorage
+let numVisits = Number(window.localStorage.getItem("visits-ls"));
+
+// determine if this is the first visit or display the number of visits.
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit!`;
+}
+
+// increment the number of visits.
+numVisits++;
+// store the new number of visits value
+localStorage.setItem("visits-ls", numVisits);
+// show todays date.
+todayDisplay.textContent = Date.now();
