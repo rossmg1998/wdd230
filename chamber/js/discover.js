69710120dc0e203bedf69c0ff62089cad24faef5
Use your GitHub username <Enter your email address>
2022-10-28 13:18:13 -0600

    Changed image load & filter

--- chamber/js/discover.js
+++ chamber/js/discover.js
@@ -1,43 +1,36 @@
 // Lazy Load Images
 
-// Gather the images to load
-let imagesToLoad = document.querySelectorAll("img[data-src]");
+const images = document.querySelectorAll('[data-src]');
 
-// Set up the load images function which switches the src and the data-src attributes
-const loadImages = (image) => {
-image.setAttribute("src", image.getAttribute("data-src"));
-image.onload = () => {
-    image.removeAttribute("data-src");
+function preloadImage(img) {
+    const source = img.getAttribute("data-src");
+    if(!source) {
+        return;
+    }
+
+    img.src = source; // change the src from placeholder to data-src of image
+}
+
+const options = {
+    threshold: 1 // 100% into viewport 
 };
-};
 
-// Load all the images
-//imagesToLoad.forEach((img) => {
-//loadImages(img);
-//});
-
-// Add an intersection observer if supported by the browser
-if ("IntersectionObserver" in window) {
-    const observer = new IntersectionObserver((items, observer) => {
-        items.forEach((item) => {
-            if (item.isIntersecting) {
-                loadImages(item.target);
-                observer.unobserve(item.target);
+const io = new IntersectionObserver (
+    (entries, io) => {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                return;
+            } else {
+                preloadImage(entry.target);
+                io.unobserve(entry.target);
             }
         });
+    }, options);
+
+    images.forEach(image => {
+        io.observe(image)
     });
 
-    imagesToLoad.forEach((img) => {
-        observer.observe(img);
-    });
-}
-
-// If the browser doesn't support intersection observers, then just load the images
-else {
-    imagesToLoad.forEach((img) => {
-        loadImages(img);
-});
-}
 
 
 // Last Visit to the Page
