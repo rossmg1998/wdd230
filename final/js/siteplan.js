d7cd0e451ee5bac861f4524815a52667c95eda06
Use your GitHub username <Enter your email address>
2022-11-29 20:02:01 -0700

    Added last modification date

---
+++ final/js/siteplan.js
@@ -0,0 +1,4 @@
+// Last Modified Date
+const today = new Date();
+document.querySelector("footer div span").textContent = today.getFullYear();
+document.getElementById("lastupdatedate").textContent = document.lastModified;
