f12971120dfe4f20bc933b1b1a264d7830c88b9b
Use your GitHub username <Enter your email address>
2022-12-12 17:26:08 -0700

    JavaScript for Services Dropdown Menu

--- final/js/script.js
+++ final/js/script.js
@@ -8,6 +8,28 @@ const x = document.getElementById("hamburgerBtn")
 x.onclick = toggleMenu;
 
 
+// Services Dropdown Menu
+
+// When the user clicks on the button, toggle between hiding and showing the dropdown content
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+
 // Last Modified Date
 const today = new Date();
 document.querySelector("footer div span").textContent = today.getFullYear();
